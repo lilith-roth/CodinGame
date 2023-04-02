214dad3cc166f93e83c35c7eebc825de209144cc
Dominik / Dominique Roth <dominik@roth.systems>
2023-04-02 10:40:44 +0200

    feat(mad-pod-racing): Checkpoint storage feature implemented

--- src/mad-pod-racing/lib.rs
+++ src/mad-pod-racing/lib.rs
@@ -4,12 +4,21 @@ macro_rules! parse_input {
     ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
 }
 
+#[derive(Clone, Copy)]
+struct Checkpoint {
+    position_x: i32,
+    position_y: i32,
+    distance_next_checkpoint: i32,
+}
+
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
 fn main() {
     let mut last_distance: i32 = 0;
+    let mut last_checkpoint_target: Option<Checkpoint> = None;
+    let mut checkpoints: Vec<Checkpoint> = vec![];
     // game loop
     loop {
         let mut input_line = String::new();
@@ -32,6 +41,37 @@ fn main() {
         // Write an action using println!("message...");
         // To debug: eprintln!("Debug message...");
         if last_distance == 0 { last_distance = next_checkpoint_dist; }
+        match &last_checkpoint_target {
+            Some(checkpoint) => {
+                if checkpoint.position_x != next_checkpoint_x || checkpoint.position_y != next_checkpoint_y {
+                    let new_checkpoint = Checkpoint {
+                        position_x: next_checkpoint_x,
+                        position_y: next_checkpoint_y,
+                        distance_next_checkpoint: 0,
+                    };
+                    last_checkpoint_target = Option::from(new_checkpoint);
+                    let already_in_vec = checkpoints.iter().find(|&&search_item| search_item.position_y == next_checkpoint_x && search_item.position_y == next_checkpoint_y);
+                    match already_in_vec {
+                        None => {
+                            checkpoints.extend([new_checkpoint]);
+                        }
+                        _ => { assert!(false, "Should not happen!"); }
+                    }
+                }
+            }
+            None => {
+                let checkpoint = Checkpoint {
+                    position_x: next_checkpoint_x,
+                    position_y: next_checkpoint_y,
+                    distance_next_checkpoint: 0,
+                };
+                last_checkpoint_target = Option::from(checkpoint);
+                checkpoints.extend([checkpoint]);
+            }
+        }
+        // if last_checkpoint_target == None {
+        //
+        // }
 
 
         // You have to output the target position
@@ -41,6 +81,7 @@ fn main() {
         if next_checkpoint_dist < 3500 {
             if next_checkpoint_dist < 250 { thrust = 5 } else { thrust = next_checkpoint_dist / 35; }
         } else if next_checkpoint_dist > 5000 {
+            // ToDo: Update to only boost on longest straight & disable boost early enough to slow down!
             println!("{} {} {}", next_checkpoint_x, next_checkpoint_y, String::from("BOOST"));
             last_distance = next_checkpoint_dist;
             continue;
