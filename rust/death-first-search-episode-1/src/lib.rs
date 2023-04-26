9606a6241b48eb410585c5d88af76d930f4c286d
Dominique Roth <dominik@roth.systems>
2023-04-26 20:19:30 +0200

    feat(death-first-search-episode-1): W.I.P.

--- rust/death-first-search-episode-1/src/lib.rs
+++ rust/death-first-search-episode-1/src/lib.rs
@@ -5,7 +5,7 @@ macro_rules! parse_input {
     ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Gateway(i32, i32);
 
 /**
@@ -19,7 +19,7 @@ fn main() {
     let n = parse_input!(inputs[0], i32); // the total number of nodes in the level, including the gateways
     let l = parse_input!(inputs[1], i32); // the number of links
     let e = parse_input!(inputs[2], i32); // the number of exit gateways
-    let mut b_map = BTreeMap::new();
+    let mut b_map: BTreeMap<i32, Vec<i32>> = BTreeMap::new();
     for i in 0..l as usize {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
@@ -29,7 +29,12 @@ fn main() {
         eprintln!("i {}", i);
         eprintln!("n1 {}", n1);
         eprintln!("n2 {}", n2);
-        b_map.insert(n2, n1);
+
+
+        let mut old_entry: Vec<i32> = Clone::clone(&b_map).get_mut(&n2)
+            .unwrap_or(&mut vec![]).to_owned();
+        old_entry.extend([n1]);
+        b_map.insert(n2, old_entry);
     }
     let mut gateways: Vec<Gateway> = vec![];
     for i in 0..e as usize {
@@ -40,7 +45,6 @@ fn main() {
         eprintln!("ei {} {}", i, ei);
         gateways.extend([Gateway(i as i32, ei)]);
     }
-    eprintln!("map {:?}", b_map);
     eprintln!("gateways {:?}", gateways);
 
     // game loop
@@ -54,9 +58,14 @@ fn main() {
 
 
         // Example: 0 1 are the indices of the nodes you wish to sever the link between
+        let gateway_item = gateways.first().unwrap();
+        eprintln!("gi {:?}", gateway_item);
+        eprintln!("map {:?}", b_map);
+        let map_item = b_map.get_mut(&gateway_item.1).unwrap().pop().unwrap();
+        eprintln!("mi {}", map_item);
         println!("{:?} {:?}",
-                 b_map.get(&gateways[0].1).unwrap(),
-                 &gateways[0].1
+                 gateway_item.1,
+                 map_item,
         );
     }
 }
