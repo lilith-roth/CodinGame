d3b7db7133fff97d84dfd155d5787725f3f08113
Dominique Roth <dominik@roth.systems>
2023-07-15 07:51:32 +0200

    code-vs-zombies: cleanup

--- rust/code-vs-zombies/src/lib.rs
+++ rust/code-vs-zombies/src/lib.rs
@@ -29,7 +29,6 @@ struct Human {
 struct Zombie {
     entity: Entity,
     position_next: Position,
-    distance_to_character: Option<i32>,
     distances_to_humans: Option<Vec<i32>>,
 }
 
@@ -44,35 +43,6 @@ impl Zombie {
             > character_distance / (CHARACTER_KILL_RANGE + CHARACTER_MOVE_SPEED)
     }
 
-    fn calculate_danger_level(
-        mut self,
-        humans: &Vec<Human>,
-        character_position: Position,
-    ) -> Zombie {
-        let distance_x_character: i32 = character_position.0 - self.entity.position.0;
-        let distance_y_character: i32 = character_position.0 - self.entity.position.0;
-        let distance_character: i32 = pythagorean_theorem(distance_x_character, distance_y_character);
-        let mut distances: Vec<i32> = vec![];
-        for human in humans.iter() {
-            let distance_x_human: i32 =
-                human.entity.position.0 - self.position_next.0;
-            let distance_y_human: i32 =
-                human.entity.position.1 - self.position_next.1;
-            let distance_human: i32 = pythagorean_theorem(distance_x_human, distance_y_human);
-
-            if distance_human / (ZOMBIE_KILL_RANGE + ZOMBIE_MOVE_SPEED) > distance_character / (CHARACTER_KILL_RANGE + CHARACTER_MOVE_SPEED) {
-                distances.extend([distance_human]);
-            }
-        }
-        self.distance_to_character = Option::from(distance_character);
-        if distances.is_empty() {
-            return self;
-        }
-        distances.sort();
-        self.distances_to_humans = Option::from(distances);
-        self
-    }
-
     fn is_zombie_targeting_entity(&self, target_entity_position: Position) -> bool {
         calculate_distance(target_entity_position, self.position_next)
             + calculate_distance(self.entity.position, self.position_next)
@@ -84,12 +54,12 @@ impl Zombie {
  * Save humans, destroy zombies!
  **/
 fn main() {
-    let mut last_target: Option<Position> = Option::None;
+    let mut last_target: Option<Position> = None;
     // game loop
     loop {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
-        let inputs = input_line.split(" ").collect::<Vec<_>>();
+        let inputs = input_line.split(' ').collect::<Vec<_>>();
         // let x = parse_input!(inputs[0], i32);
         // let y = parse_input!(inputs[1], i32);
         let character_position: Position = Position(
@@ -100,10 +70,10 @@ fn main() {
         io::stdin().read_line(&mut input_line).unwrap();
         let human_count = parse_input!(input_line, i32);
         let mut humans: Vec<Human> = vec![];
-        for i in 0..human_count as usize {
+        for _i in 0..human_count as usize {
             let mut input_line = String::new();
             io::stdin().read_line(&mut input_line).unwrap();
-            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let inputs = input_line.split(' ').collect::<Vec<_>>();
             let human_id = parse_input!(inputs[0], i32);
             let human_x = parse_input!(inputs[1], i32);
             let human_y = parse_input!(inputs[2], i32);
@@ -113,7 +83,7 @@ fn main() {
                     position: Position(human_x, human_y),
                     is_human: true,
                 },
-                zombies_facing_human: Option::None,
+                zombies_facing_human: None,
             };
             humans.extend([new_entity]);
         }
@@ -121,32 +91,24 @@ fn main() {
         io::stdin().read_line(&mut input_line).unwrap();
         let zombie_count = parse_input!(input_line, i32);
         let mut zombies: Vec<Zombie> = vec![];
-        for i in 0..zombie_count as usize {
+        for _i in 0..zombie_count as usize {
             let mut input_line = String::new();
             io::stdin().read_line(&mut input_line).unwrap();
-            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let inputs = input_line.split(' ').collect::<Vec<_>>();
             let zombie_id = parse_input!(inputs[0], i32);
             let zombie_x = parse_input!(inputs[1], i32);
             let zombie_y = parse_input!(inputs[2], i32);
             let zombie_xnext = parse_input!(inputs[3], i32);
             let zombie_ynext = parse_input!(inputs[4], i32);
-            let new_entity = Zombie {
+            let new_zombie = Zombie {
                 entity: Entity {
                     id: zombie_id,
                     position: Position(zombie_x, zombie_y),
                     is_human: false,
                 },
                 position_next: Position(zombie_xnext, zombie_ynext),
-                distance_to_character: Option::None,
-                distances_to_humans: Option::None,
+                distances_to_humans: None,
             };
-            let new_zombie = new_entity.calculate_danger_level(
-                &humans,
-                Position(
-                    character_position.0,
-                    character_position.1,
-                ),
-            );
             zombies.extend([new_zombie])
         }
 
@@ -155,32 +117,23 @@ fn main() {
                 .cmp(&calculate_distance(a.entity.position, character_position)));
 
         let mut target: Option<Position> = None;
-        // for zombie in zombies.iter() {
-        //     if zombie.is_zombie_targeting_entity(character_position) {
-        //         target = Option::from(zombie.entity.position);
-        //         break;
-        //     }
-        // }
 
         humans.sort_by(|a, b|
             calculate_distance(b.entity.position, character_position)
-            .cmp(&calculate_distance(a.entity.position, character_position)));
+                .cmp(&calculate_distance(a.entity.position, character_position)));
 
-        let humansLoopVec: Vec<Human> = humans.clone();
-        'outer: for human in humansLoopVec.iter() {
-            let mut new_human: Human = human.clone();
+        let humans_loop_vec: Vec<Human> = humans.clone();
+        'outer: for human in humans_loop_vec.iter() {
+            let mut new_human: Human = *human;
             for zombie in zombies.iter() {
                 if !zombie.clone().can_character_save_in_time(character_position, new_human.entity.position) {
                     let index = humans.iter().position(|x| x == human).unwrap();
                     humans.remove(index);
-                    continue 'outer
+                    continue 'outer;
                 }
 
-                // eprintln!("Dist {}", calculate_distance(new_human.entity.position, zombie.entity.position));
-                if (zombie.is_zombie_targeting_entity(new_human.entity.position)
-                    /*|| is_in_distance(new_human.entity.position, zombie.entity.position, 3000)*/
-                    /*|| is_in_distance(character_position, zombie.entity.position, 3000) */)
-                     {
+                if zombie.is_zombie_targeting_entity(new_human.entity.position)
+                {
                     eprintln!("Potential target {:?}", new_human.entity.position);
                     match new_human.zombies_facing_human {
                         Some(amount) => new_human = {
@@ -196,7 +149,7 @@ fn main() {
                     }
                 }
             }
-            let check_human: Human = new_human.clone();
+            let check_human: Human = new_human;
             eprintln!("Facing: {}", check_human.zombies_facing_human.unwrap_or(-1));
             if check_human.zombies_facing_human.unwrap_or(-1) > 0 {
                 target = Option::from(check_human.entity.position);
@@ -230,7 +183,7 @@ fn main() {
                         )
                     }
                     None => {
-                        if (humans.len() > 0) {
+                        if !humans.is_empty() {
                             target = Option::from(humans[0].entity.position);
                         } else {
                             target = Option::from(zombies[0].entity.position);
@@ -248,7 +201,7 @@ fn main() {
     }
 }
 
-fn pythagorean_theorem(a: i32, b: i32) -> i32 {
+fn calculate_pythagorean_theorem(a: i32, b: i32) -> i32 {
     ((a.pow(2) + b.pow(2)) as f32).sqrt() as i32
 }
 
@@ -257,5 +210,5 @@ fn calculate_distance(a: Position, b: Position) -> i32 {
 }
 
 fn is_in_distance(a: Position, b: Position, distance: i32) -> bool {
-    (distance < calculate_distance(a, b))
+    distance < calculate_distance(a, b)
 }
